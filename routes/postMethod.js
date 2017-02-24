0700513f961db980636c2ffcd00187f6d38114d4
Boat SaGo <Boat SaGo>
2017-02-24 23:02:23 +0700

    Zako changes

--- routes/postMethod.js
+++ routes/postMethod.js
@@ -5,6 +5,7 @@
 var postRoutes = {};
 
 postRoutes['/createPlayer'] = require('./post/create_player.js').getPage;
+postRoutes['/selectLevel'] = require('./post/select_level.js').getPage;
 
 postRoutes['Error 404'] = (req, res) => {
   res.end('Error 404 : Page not found!!');
