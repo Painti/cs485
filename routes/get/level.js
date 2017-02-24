c0734c1b5ef228086c9e0207bd39699ae4be945d
Boat SaGo <Boat SaGo>
2017-02-24 22:01:44 +0700

    Zako changes

--- routes/get/level.js
+++ routes/get/level.js
@@ -1,5 +1,18 @@
+var fs = require('fs');
+
 module.exports = {
     getPage: (req, res) => {
-        res.end('level');
+        var data = fs.readFileSync('./view/level.html');
+        data += '<ul>';
+        for (var i = 0; i < 10; i++) {
+          data += '<li>level ' + i + '</li>';
+        }
+        data += '</ul></body></html>';
+        res.writeHead(200, {
+          'Content-Type': 'text/html',
+          'Content-Length': data.length
+        });
+        res.write(data);
+        res.end();
     }
 };
