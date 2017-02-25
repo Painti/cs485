4d80a071edecfc735c57bc355461061c931dc849
Boat SaGo <Boat SaGo>
2017-02-25 17:44:00 +0700

    Zako changes

--- routes/get/play.js
+++ routes/get/play.js
@@ -1,5 +1,12 @@
+var fs = require('fs');
+
 module.exports = {
-    getPage: (req, res) => {
-        res.end('play');
-    }
+  getPage: (req, res) => {
+      var data = fs.readFileSync('./view/play.html')
+      res.writeHead(200, {
+          'Content-Type': 'text/html',
+          'Content-Length': data.length
+      });
+      res.end(data);
+  }
 };
