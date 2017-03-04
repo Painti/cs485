c8cbd5b99cad93ca7e6ab58df0b7b5e4da8eb4a5
winner0660 <winjibi_@hotmail.com>
2017-03-04 17:20:12 +0700

    Baroad

--- routes/post/create_player.js
+++ routes/post/create_player.js
@@ -1,5 +1,5 @@
 var qs = require('querystring');
-
+var fs = require('fs');
 module.exports = {
   getPage: (req, res) => {
     var body = '';
@@ -8,11 +8,23 @@ module.exports = {
     });
     req.on('end', function() {
       var post = qs.parse(body);
-      res.setHeader('Set-Cookie', 'name=' + encodeURI(post.name));
-      res.writeHead(302, {
-        'Location': '/level'
-      });
-      res.end();
+      var name = (post.name).replace(/ /g,'');
+      if(name != ''){
+        res.setHeader('Set-Cookie', 'name=' + encodeURI(post.name));
+        res.writeHead(302, {
+          'Location': '/level'
+        });
+        res.end();
+      }
+      else{
+        var data = fs.readFileSync('./view/index.html');
+        data += '<script>window.alert("Please input your name");</script>';
+        res.writeHead(200, {
+          'Content-Type': 'text/html',
+          'Content-Length': data.length
+        });
+        res.end(data);
+      }
     });
 
   }
