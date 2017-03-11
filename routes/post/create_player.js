27d523c978e5f327044f7408c1565588514e3b57
Boat SaGo <Boat SaGo>
2017-03-11 23:24:39 +0700

    winjibi

--- routes/post/create_player.js
+++ routes/post/create_player.js
@@ -8,23 +8,11 @@ module.exports = {
     });
     req.on('end', function() {
       var post = qs.parse(body);
-      var name = (post.name).replace(/ /g,'');
-      if(name != ''){
-        res.setHeader('Set-Cookie', 'name=' + encodeURI(post.name));
-        res.writeHead(302, {
-          'Location': '/level'
-        });
-        res.end();
-      }
-      else{
-        var data = fs.readFileSync('./view/index.html');
-        data += '<script>window.alert("Please input your name");</script>';
-        res.writeHead(200, {
-          'Content-Type': 'text/html',
-          'Content-Length': data.length
-        });
-        res.end(data);
-      }
+      res.setHeader('Set-Cookie', 'name=' + encodeURI(post.name));
+      res.writeHead(302, {
+        'Location': '/level'
+      });
+      res.end();
     });
   }
 };
